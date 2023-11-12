cc29ed37a1ce07b3904469b233342708c3241b35
Rampat3 <dennyfells13@gmail.com>
2023-11-12 17:21:03 +0700

    update headers/added section promo-header

---
+++ src/contents/NavMenu.php
@@ -0,0 +1,85 @@
+<div class="NavMenu">
+    <div class="NavMenu__icons">
+        <img src="../src/icons/icon.png" alt="">
+        <a href="#">Academy</a>
+    </div>
+    <div class="NavMenu__categories">
+        <a href="#">Категории</a>
+    </div>
+    <div class="NavMenu__search">
+        <form>
+            <input type="text" placeholder="Ищите что угодно">
+            <button type="submit">Поиск</button>
+        </form>
+    </div>
+    <div class="NavMenu__authorization">
+        <div class="NavMenu__authorization-login">
+            <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Войти
+            </button>
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Войти</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <form>
+                                <div class="mb-3">
+                                    <label for="exampleInputEmail1" class="form-label">Email адрес</label>
+                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleInputPassword1" class="form-label">Пароль</label>
+                                    <input type="password" class="form-control" id="exampleInputPassword1">
+                                </div>
+                            </form>
+                        </div>
+                        <div class="modal-footer" style="display: flex; justify-content: center;">
+                            <button type="button" class="btn btn-dark">Авторизоваться</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="NavMenu__authorization-register">
+            <button type="button" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#exampleModal1">
+                Регистрация
+            </button>
+            <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Регистрация</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <form>
+                                <div class="mb-3">
+                                    <label for="exampleInputEmail1" class="form-label">Имя</label>
+                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleInputEmail1" class="form-label">Номер телефона</label>
+                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleInputEmail1" class="form-label">Email адрес</label>
+                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleInputPassword1" class="form-label">Пароль</label>
+                                    <input type="password" class="form-control" id="exampleInputPassword1">
+                                </div>
+                            </form>
+                        </div>
+                        <div class="modal-footer" style="display: flex; justify-content: center;">
+                            <button type="button" class="btn btn-dark">Зарегистрироваться</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ src/contents/promo-header.php
@@ -0,0 +1,34 @@
+<div class="promo-header__img">
+    <img src="../src/img/bg-header.jpg" alt="" width="100%">
+</div>
+<div class="promo-header__text">
+    <div class="promo-header__text-bg">
+        <div class="title__text">
+            <p>
+                Большой выбор курсов
+            </p>
+        </div>
+        <div class="title__text-p">
+            <p>
+                Выбирайте из более 210 000 онлайн-видеокурсов, новые курсы добавляют на сайт каждый месяц
+            </p>
+        </div>
+    </div>
+</div>
+<div class="promo-header__companies">
+    <div class="promo-header__companies-text title__text-p">
+        Нам доверяют более 15 000 компаний и миллионы учащихся по всему миру
+    </div>
+    <div class="promo-header__companies-img">
+        <ul>
+            <li><img src="../src/icons/companies/volkswagen.svg" alt=""></li>
+            <li><img src="../src/icons/companies/samsung.svg" alt=""></li>
+            <li><img src="../src/icons/companies/cisco.svg" alt=""></li>
+            <li><img src="../src/icons/companies/att.svg" alt=""></li>
+            <li><img src="../src/icons/companies/procter_gamble.svg" alt=""></li>
+            <li><img src="../src/icons/companies/hewlett_packard_enterprise.svg" alt=""></li>
+            <li><img src="../src/icons/companies/citi.svg" alt=""></li>
+            <li><img src="../src/icons/companies/ericsson.svg" alt=""></li>
+        </ul>
+    </div>
+</div>
