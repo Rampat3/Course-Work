c5d2932ebbf4b59dfbcd9894726b1992e15a1aca
Rampat3 <dennyfells13@gmail.com>
2023-11-14 20:50:31 +0700

    Added footer

---
+++ src/contents/footer.php
@@ -0,0 +1,12 @@
+<div class="footer">
+    <div class="footer__body">
+        <ul>
+            <li>
+                Academy
+            </li>
+            <li>
+                © 2023 Udemy, Inc.
+            </li>
+        </ul>
+    </div>
+</div>
