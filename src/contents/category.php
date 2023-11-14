40a8d473f8178e522cc238ad13a4bc482cdd5907
Rampat3 <dennyfells13@gmail.com>
2023-11-14 20:12:08 +0700

    Added new block - Category

---
+++ src/contents/category.php
@@ -0,0 +1,57 @@
+<div class="category__body">
+    <div class="category__body-header">
+        <p class="title__text">Популярные категории</p>
+    </div>
+    <div class="category__body-list">
+        <ul>
+            <li>
+                <a href="">
+                    <img src="../src/img/category/design.jpg" alt="">
+                    <p>Дизайн</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="../src/img/category/devolper.jpg" alt="">
+                    <p>Разработка</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="../src/img/category/marketing.jpg" alt="">
+                    <p>Маркетинг</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="../src/img/category/it.jpg" alt="">
+                    <p>ИТ и ПО</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="../src/img/category/personal.jpg" alt="">
+                    <p>Личностный рост</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="../src/img/category/business.jpg" alt="">
+                    <p>Бизнес</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="../src/img/category/photo.jpg" alt="">
+                    <p>Фотография</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="../src/img/category/music.jpg" alt="">
+                    <p>Музыка</p>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
